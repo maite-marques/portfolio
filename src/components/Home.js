838b3057da5d1554be995ee404256237d3185b55
maite-marques <maite.marques184@gmail.com>
2022-08-17 09:27:00 -0300

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 import { Email, Github, Linkedin } from '../images';
 import './Home.css';
+import Octocat from '../image/Octocat.png';
 
 function Home() {
   return (
     <div className="home">
-      <section>
-
+      <section className='section section-profile'>
+        <img className="profile-img"src={Octocat} alt="octocat" />
       </section>
-      <section>
+      <section className='section section-content'>
         <h1>Hello, I'm Maitê</h1>
         <h3>I'm a front end developer</h3>
-        <img src={ Linkedin} alt="linkedin" />
-        <img src={ Github } alt="github" />
-        <img src={ Email } alt="email" />
+        <div className='logo-img-container'>
+          <img className="logo-img" src={ Linkedin} alt="linkedin" />
+          <img className="logo-img" src={ Github } alt="github" />
+          <img className="logo-img" src={ Email } alt="email" />
+        </div>
       </section>
     </div>
   )
